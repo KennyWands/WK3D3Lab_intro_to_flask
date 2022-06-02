8476e487e370b9f16b2db546a56df8a497b33565
Kenny Wands <kjwands@gmail.com>
2022-06-02 09:49:50 +0100

    working. add CSS

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask
 
 app = Flask(__name__)
-
+#creates app object of class Flask
 from controllers import controller
 
 if __name__ == "__main__":
     app.run(debug=True)
+# name is predefined variable and sets main as the module in which flask 
+# is used. app.run then is the controller module
--- controllers/controller.py
+++ controllers/controller.py
@@ -1,13 +1,18 @@
 from webbrowser import get
-from flask import render_template
+from flask import render_template  # associates the html pages with the flask app
 
-from app import app
+from app import app  # our instance of flask
 from models.order_list import *
 
 
-@app.route("/orders")
-def index():
-    return render_template("index.html", title="home", order_list=orders)
+@app.route("/orders")  # routes implement different urls. this known as a decorator,
+# adds extra functionality to function
+# this decorator associates url with the function below
+def index():  # view function
+    return render_template("index.html", title="home", orders=orders)
+    # index template, title,
+    # render-temp part of jinja2 template engine gives the {{ }} functionality
+    # arguments passed here will go to the html in the {{ }}sections
 
 
 # @app.route("/orders/0")
@@ -15,6 +20,7 @@ def index():
 #     return render_template("order.html", title="home", order_list=orders[0])
 
 
-@app.route("/orders/index", methods=["get"])
-def order():
-    return render_template("order.html", title="home", print=orders)
+@app.route("/order/<index>")
+def order(index):
+    chosen_order= orders[int(index)]
+    return render_template("order.html", order= chosen_order)
